ba4ac7c46af988304c4eef2d0e338801c7705baf
Chad Schulz <chad.awesome@gmail.com>
2018-10-19 21:46:24 -0700

    Add function

--- parse-sitemap.cs
+++ parse-sitemap.cs
@@ -7,7 +7,7 @@ namespace Sitemap
 {
     public static class ParseSitemap
     {
-        [FunctionName("parse_sitemap")]
+        [FunctionName("parse-sitemap")]
         public static void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
