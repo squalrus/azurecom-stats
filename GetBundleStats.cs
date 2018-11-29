2d796fece16012e1a890cbc6513cc0b6ed5151f5
Chad Schulz <chad.awesome@gmail.com>
2018-11-28 22:08:12 -0800

    Add charts

--- GetBundleStats.cs
+++ GetBundleStats.cs
@@ -24,7 +24,7 @@ namespace AzurecomStatsFunctions
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             TableQuery<BundleData> rangeQuery = new TableQuery<BundleData>().Where(
-                TableQuery.GenerateFilterConditionForDate("Timestamp", QueryComparisons.GreaterThanOrEqual, DateTimeOffset.UtcNow.AddDays(-14))
+                TableQuery.GenerateFilterConditionForDate("Timestamp", QueryComparisons.GreaterThanOrEqual, DateTimeOffset.UtcNow.AddDays(-30))
             );
 
             List<BundleData> bundleData = new List<BundleData>();
