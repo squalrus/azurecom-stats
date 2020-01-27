5bee8aec37c978490b3fe804a6674df3467e878c
Chad Schulz <chad.awesome@gmail.com>
2020-01-26 22:40:19 -0800

    Update sitemap data and chart rendering

--- Shared/SitemapData.cs
+++ Shared/SitemapData.cs
@@ -7,9 +7,15 @@ namespace AzurecomStatsFunctions.Shared
         public string FriendlyDate => Timestamp.DateTime.ToString("MMMM dd, yyyy hh:mm:ss");
         public int UrlCount { get; set; }
         public int UniqueUrlCount { get; set; }
+
         public int BlogUrlCount { get; set; }
+        public int EventsUrlCount { get; set; }
+        public int KnowledgeCentersUrlCount { get; set; }
+        public int PodcastsUrlCount { get; set; }
+        public int PricingDetailsUrlCount { get; set; }
         public int ResourcesUrlCount { get; set; }
-        public int SamplesUrlCount { get; set; }
+        public int ServicesUrlCount { get; set; }
+        public int SolutionArchitecturesUrlCount { get; set; }
         public int TemplatesUrlCount { get; set; }
         public int UpdatesUrlCount { get; set; }
         public int VideoUrlCount { get; set; }
