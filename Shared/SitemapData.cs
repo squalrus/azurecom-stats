a246adea816f466d81d2aa6d78fbcfd3e3d2f53b
Chad Schulz <chad.awesome@gmail.com>
2022-01-24 13:00:29 -0800

    Remove now unused items from the UI (#18)

--- Shared/SitemapData.cs
+++ Shared/SitemapData.cs
@@ -9,16 +9,11 @@ namespace AzurecomStatsFunctions.Shared
         public int UniqueUrlCount { get; set; }
 
         public int BlogUrlCount { get; set; }
-        public int EventsUrlCount { get; set; }
-        public int KnowledgeCentersUrlCount { get; set; }
         public int PodcastsUrlCount { get; set; }
         public int PricingDetailsUrlCount { get; set; }
         public int ResourcesUrlCount { get; set; }
         public int ServicesUrlCount { get; set; }
-        public int SolutionArchitecturesUrlCount { get; set; }
         public int TemplatesUrlCount { get; set; }
         public int UpdatesUrlCount { get; set; }
-        public int VideoUrlCount { get; set; }
     }
-
 }
